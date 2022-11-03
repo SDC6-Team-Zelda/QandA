6cb790c25f398e4b136bcbe8ed65d34615d416e4
JustinTurkaly <jturkaly18@gmail.com>
2022-11-03 15:51:00 -0500

    Added query for first get request

---
+++ db/controllers.js
@@ -0,0 +1,20 @@
+const questionFormatter = (q, a) => {
+// console.log(q, a)
+let result = []
+// console.log(a)
+for (let i = 0; i < q.length; i++) {
+  let id = q[i].id
+  let dataObj = q[i]
+  let answerArray = []
+  dataObj.answers = []
+  for (let j = 0; j < a.length; j++) {
+    if (a[j].question_id === id) {
+      dataObj.answers.push(a[j])
+    }
+  }
+  result.push(dataObj)
+}
+console.log(result)
+}
+
+module.exports.questionFormatter = questionFormatter;
--- db/index.js
+++ db/index.js
@@ -1,4 +1,6 @@
 const { Client } = require('pg');
+const {questionFormatter} = require('./controllers.js')
+
 const connection = {
   host: 'localhost',
   port: 5432,
@@ -8,11 +10,37 @@ const connection = {
 };
 const client = new Client(connection);
 client.connect()
-client.query('SELECT * FROM answers_photos', (err, res) => {
+client.query('select * from questions where product_id = 5', (err, res) => {
   if (err) {
     console.log(err)
   } else {
-    console.log(res)
+    // console.log(res.rows)
+    for (let i = 0; i < res.rows.length; i++) {
+      let id = res.rows[i].id;
+      client.query(`select * from answers where question_id = ${id}`, (er, re) => {
+        if (er) {
+          console.log(er)
+        } else {
+          // console.log('questions', res.rows, 'answers', re.rows)
+          questionFormatter(res.rows, re.rows)
+          // let dataObj = {}
+          // for (let i = 0; i < res.rows.length; i++) {
+          //   let link = res.rows[i].id;
+          //   let result = []
+          //   for (let j = 0; j < re.rows.length; j++) {
+          //     // console.log(re.rows[j]
+          //     if (re.rows[j].question_id.toString() === link.toString()) {
+          //       result.push(re.rows[j])
+          //     }
+          //   }
+          //   let key = res.rows[i]
+          //   dataObj[key] = result;
+          // }
+          // console.log(dataObj)
+          client.end()
+        }
+      })
+    }
   }
-  client.end()
 })
+
